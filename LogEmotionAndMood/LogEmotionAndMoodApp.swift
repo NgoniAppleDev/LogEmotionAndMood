840e3fb86733bb940c1692f94e34c0e7a9fa2115
NgoniAppleDev <ngonikatsidzira@icloud.com>
2025-02-05 11:07:38 +0200

    Initial Commit

---
+++ LogEmotionAndMood/LogEmotionAndMoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogEmotionAndMoodApp.swift
+//  LogEmotionAndMood
+//
+//  Created by Ngoni Katsidzira  on 5/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct LogEmotionAndMoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
